c8a575dbf8d09727d5ea6f69c2f9999a1b1c04dc
zhaoxinxi <90zhaoxinxi@gmail.com>
2017-05-18 15:52:08 +0800

    修改seed生成测试数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,24 @@ u.password = '123456'
 u.password_confirmation = '123456'
 u.is_admin = true
 u.save
+
+u = User.new
+u.email = "user@test.com"
+u.password = '123456'
+u.password_confirmation = '123456'
+u.is_admin = false
+u.save
+
+f_title = ["商品1","商品2","商品3","商品4","商品5","商品6"]
+f_uantity = [10,20,30,10,20,20]
+f_price = [50,50,100,100,200,200]
+
+for i in 0..5 do
+  Product.create!(
+  title: f_title [i],
+  description: "测试用的商品描述",
+  quantity: f_uantity [i],
+  price: f_price [i],
+  image: nil
+  )
+end
