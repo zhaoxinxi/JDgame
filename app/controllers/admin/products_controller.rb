e823475b38d3af801a701365ee305231fff9c1d0
zhaoxinxi <90zhaoxinxi@gmail.com>
2017-05-22 23:18:46 +0800

    merge

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,10 +6,14 @@ class Admin::ProductsController < ApplicationController
 
   def index
     @products=Product.all
+    if params[:category].present?                     # 这里是为了给首页加分类筛选条件
+      @products = @products.where(category_id: params[:category])
+    end
   end
 
   def new
     @product=Product.new
+    @categories = Category.all.map{|c| [c.name,c.id]}  #加上这句，map的作用很关键，转换成数组，建立应射
   end
 
   def create
@@ -27,6 +31,7 @@ class Admin::ProductsController < ApplicationController
 
   def edit
     @product=Product.find(params[:id])
+    @categories = Category.all.map{|c| [c.name,c.id]}   # 同样加上这句
   end
 
   def update
@@ -47,7 +52,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title,:description,:price,:quantity,:image)
+    params.require(:product).permit(:title,:description,:price,:quantity,:image,:category_id)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
+    resources :categories
     resources :products
     resources :orders do
       member do
